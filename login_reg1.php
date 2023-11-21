0c0b7515359336fe162fb56f642ad91290e5a904
Elshod <elshodatc1116@gmail.com>
2023-11-21 18:56:31 +0500

    Registratsiya uchun SMS yuborish kerak

--- login_reg1.php
+++ login_reg1.php
@@ -3,13 +3,17 @@
     $name = $_GET['name'];
     $Phone = str_replace(" ","",$_GET['phone']);
     $str1 = chunk_split($Phone,8,".");
+    $str3 = str_replace("998 ","",$_GET['phone']);
+    $str4 = str_replace(" ","",$str3);
     $str2 = explode(".",$str1);
     $Phone1 = $str2[1];
     $Random1 = rand(10,99);
     $Random2 = rand(10,99);
-    echo "Phone: ".$Phone."<br>";
+    echo "Phone 998956565: ".$Phone."<br>";
     echo "Random: ".$Random1." ".$Random2."<br>";
-    echo "Phone oxiri: ".$Phone1." ";
+    echo "Phone oxiri: ".$Phone1." <br>";
+    echo $str4;
+
     $Tasdiqlash = "Tasdiqlash kodi: ".$Random1." ".$Random1."\n ATKO o'quv markazi. Ma'lumot uchun: (91) 950 1101";
 
 
@@ -20,8 +24,24 @@
         header("location: ./login_reg.php?registr=true");
     }else{
         echo "SMS yuborish uchun tayyor bolgan SMS yuborishi kerak";
-        
+        sendMessege($str4,$Tasdiqlash);
     }
-
+    function sendMessege($text,$phone){
+        $data = json_encode([
+            'send'=>'',
+            'number'=>$phone,
+            'text'=>$text,
+            'user_id'=>'5139864291',
+            'token'=>"emNlVoGxdivUfMjAKYXPIORyDFLkqtSsnEZHarTQuBhbJgp",
+            'id'=>'5390'
+        ]);
+        $url = "https://api.xssh.uz/smsv1/?data=".urlencode($data);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        $res = curl_exec($ch);
+        $smsArray = json_decode($res, true);
+        echo "oke";
+    }
 
 ?>
